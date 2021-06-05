82f54e51404a8837730be6c5f4212288552fa460
Xarvveron <xarvveron@protonmail.com>
2021-06-05 22:53:36 +0000

    Fixed pycache in .gitignore, created csv file, and various functions for the basic beginnnings of character creation.

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import random
+from utils import append_list_as_row as csv_append
+
+classes = [
+    "Barbarian", "Bard", "Cleric",
+    "Druid", "Fighter", "Monk",
+    "Paladin", "Ranger", "Rogue",
+    "Sorceror", "Warlock", "Wizard"
+]
+races   = [
+    "Dragonborn", "Dwarf", "Elf",
+    "Gnome", "Half-Elf", "Halfling",
+    "Half-Orc", "Human", "Tiefling",     
+]
+
+def determine_race():
+    return random.choice(races)
+
+def determine_class():
+    return random.choice(classes)
 
 def roll_stat():
     return random.randint(9, 18)    
@@ -6,6 +25,41 @@ def roll_stat():
 def roll_stats():
     stats = []
     for i in range(0, 6): stats.append(roll_stat())
+    return stats
+
+def alter_stats(stats, race):
+    if race == races[0]:
+        stats[0] += 2
+        stats[5] += 1
+    elif race == races[1]:
+        stats[2] += 2
+    elif race == races[2]:
+        stats[1] += 2
+    elif race == races[3]:
+        stats[3] += 2
+    elif race == races[4]:
+        stats[5] += 2
+        while True:
+            n = random.randint(1, len(stats)-1)
+            if n != 5:
+                stats[n] += 1
+                break
+        while True:
+            m = random.randint(1, len(stats)-1)
+            if m != 5 and m != n:
+                stats[m] += 1
+                break
+        input()
+    elif race == races[5]:
+        stats[1] += 2
+    elif race == races[6]:
+        stats[0] += 2
+        stats[2] += 1
+    elif race == races[7]:
+        stats = [stat + 1 for stat in stats]
+    elif race == races[8]:
+        stats[5] += 2
+        stats[3] += 1
     return stats
 
 def determine_mods(stats):
@@ -27,10 +81,81 @@ def determine_mods(stats):
         elif stat >= 26 and stat <= 27: mod = 8
         elif stat >= 28 and stat <= 29: mod = 9
         elif stat == 30: mod = 10
-        else: mod = None
+        else: return "Error"
         mods.append(mod)
     return mods
 
-stats = roll_stats()
-mods = determine_mods(stats)
-print(stats, mods)
+def log_character(*args):
+    char_info = []
+    for arg in args:
+        char_info.append(arg)
+    csv_append('characters.csv', char_info)
+
+def create_character():
+    race = determine_race()
+    character_class = determine_class()
+    stats = alter_stats(roll_stats(), race)
+    mods = determine_mods(stats)
+    log_character(race, character_class, stats, mods)
+    return race, character_class, stats, mods
+
+while True:
+    create_character()
+    input()
+
+'''
+Dragonborn
+
+Racial Traits
++2 Strength, +1 Charisma, Draconic Ancestry, Breath Weapon, Damage Resistance
+View Dragonborn Details
+--------------------------------------------------------------------------------------------------------
+Dwarf
+
+Racial Traits
++2 Constitution, Darkvision, Dwarven Resilience, Dwarven Combat Training, Stonecunning
+View Dwarf Details
+--------------------------------------------------------------------------------------------------------
+Elf
+
+Racial Traits
++2 Dexterity, Darkvision, Keen Senses, Fey Ancestry, Trance
+View Elf Details
+--------------------------------------------------------------------------------------------------------
+Gnome
+
+Racial Traits
++2 Intelligence, Darkvision, Gnome Cunning
+View Gnome Details
+--------------------------------------------------------------------------------------------------------
+Half-Elf
+
+Racial Traits
++2 Charisma, +1 to Two Other Ability Scores, Darkvision, Fey Ancestry, Skill Versatility
+View Half-Elf Details
+--------------------------------------------------------------------------------------------------------
+Halfling
+
+Racial Traits
++2 Dexterity, Lucky, Brave, Halfling Nimbleness
+View Halfling Details
+--------------------------------------------------------------------------------------------------------
+Half-Orc
+
+Racial Traits
++2 Strength, +1 Constitution, Darkvision, Menacing, Relentless Endurance, Savage Attacks
+View Half-Orc Details
+--------------------------------------------------------------------------------------------------------
+Human
+
+Racial Traits
++1 to All Ability Scores, Extra Language
+View Human Details
+--------------------------------------------------------------------------------------------------------
+Tiefling
+
+Racial Traits
++2 Charisma, +1 Intelligence, Darkvision, Hellish Resistance, Infernal Legacy
+
+str, dex, con, int, wis, cha
+'''
